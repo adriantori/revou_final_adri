153778356ae6da72bb08b832b7cfb3cdcd37d5a9
Adri Antori <adriantori11@gmail.com>
2023-12-20 17:10:08 +0700

    update: polishing code and margins in Input Bencana

--- src/containers/DisasterInput/index.tsx
+++ src/containers/DisasterInput/index.tsx
@@ -11,6 +11,7 @@ import { ImageUploadComponent, MapComponent, Loading } from '../../components';
 import { useNotification } from '../../contexts/NotificationContext';
 import axios from 'axios';
 import { baseUrlDis } from '../../configs/Constants';
+import { TextareaAutosize } from '@mui/material';
 
 interface Location {
   latitude: number;
@@ -120,14 +121,14 @@ const DisasterInput = () => {
         DIS_IMAGE: formData.uploadedImageUrl,
         DIS_TIME: Date.now()
       };
-  
+
       // Make the Axios POST request
-      const response = await axios.post(`${baseUrlDis}/`, requestData,{
+      const response = await axios.post(`${baseUrlDis}/`, requestData, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
       });
-  
+
       // Handle the response, show success message, etc.
       console.log('Server Response:', response.data);
       showNotification('success', 'Laporan berhasil disampaikan!', 'Terima kasih atas laporannya');
@@ -136,43 +137,71 @@ const DisasterInput = () => {
       showNotification('error', 'Gagal mengirim laporan!', 'Terjadi kesalahan saat mengirim data');
     } finally {
       setLoading(false);
+      navigate('/laporkan')
     }
   };
 
   if (loading) {
     // If loading is true, render the Loading component
     return <Loading />;
-  } 
+  }
 
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         {/* Image Upload */}
-        <Typography variant="h6">Upload Gambar</Typography>
+        <Typography variant="h6" sx={{ marginTop: 2 }}>
+          Upload Gambar
+        </Typography>
         <ImageUploadComponent onImageUrlChange={handleImageUrlChange} />
 
         {/* Disaster */}
-        <Typography variant="h6">Judul Bencana</Typography>
-        <TextField label="Bencana" fullWidth onChange={(e) => setFormData((prevData) => ({ ...prevData, disasterTitle: e.target.value }))} />
+        <Typography variant="h6" sx={{ marginTop: 2 }}>
+          Judul Bencana
+        </Typography>
+        <TextField
+          label="Contoh: Banjir Bandang"
+          fullWidth
+          onChange={(e) => setFormData((prevData) => ({ ...prevData, disasterTitle: e.target.value }))}
+          sx={{ marginTop: 1 }}
+        />
 
         {/* Description */}
-        <Typography variant="h6">Deskripsi Bencana</Typography>
-        <TextField label="Deskripsi" fullWidth onChange={(e) => setFormData((prevData) => ({ ...prevData, description: e.target.value }))} />
+        <Typography variant="h6" sx={{ marginTop: 2 }}>
+          Deskripsi Bencana
+        </Typography>
+        <TextareaAutosize
+          aria-label="Deskripsi & Bantuan yang dibutuhkan"
+          minRows={3}
+          placeholder="Tulis deskripsi dan bantuan yang dibutuhkan di sini..."
+          style={{ width: '100%', marginTop: 1 }}
+          onChange={(e) => setFormData((prevData) => ({ ...prevData, description: e.target.value }))}
+        />
 
         {/* Donation */}
-        <Typography variant="h6">Link Donasi (Ayobantu.com / Kitabisa.com)</Typography>
-        <TextField label="Link Donasi (Jika Ada)" fullWidth onChange={(e) => setFormData((prevData) => ({ ...prevData, donationLink: e.target.value }))} />
+        <Typography variant="h6" sx={{ marginTop: 2 }}>
+          Link Donasi (Ayobantu.com / Kitabisa.com)
+        </Typography>
+        <TextField
+          label="Link Donasi (Jika Ada)"
+          fullWidth
+          onChange={(e) => setFormData((prevData) => ({ ...prevData, donationLink: e.target.value }))}
+          sx={{ marginTop: 1 }}
+        />
 
         {/* Location Detection */}
-        <Typography variant="h6">Deteksi Lokasi (izinkan lokasi)</Typography>
+        <Typography variant="h6" sx={{ marginTop: 2 }}>
+          Deteksi Lokasi (izinkan lokasi)
+        </Typography>
         <MapComponent onLocationChange={handleLocationChange} />
 
         {/* Submit Button */}
-        <Button variant="contained" color="error" onClick={handleSubmit}>
+        <Button variant="contained" color="error" onClick={handleSubmit} sx={{ marginTop: 3 }}>
           LAPORKAN
         </Button>
       </CardContent>
     </Card>
+
   );
 };
 
